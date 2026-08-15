43afa60ce430580cad42bdd796bb2494c5f60845
Tabias Pittman <tabiaspittman94@gmail.com>
2026-08-15 12:46:21 -0400

    chore: remove unused test dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -37,10 +36,9 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:1.6.1")
     implementation("androidx.media3:media3-exoplayer-dash:1.6.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.6.1")
-    testImplementation("junit:junit:4.13.2")
 }
 
-// Set the archivesBaseName using the public API (requires the base plugin to be applied)
+// Set the archivesName using the public API (requires the base plugin to be applied)
 base {
     archivesName = pluginName
 }
